da2ebbf46192716bf7630e807cada67f3b82204e
mikelhpdatke <mikelhpdatke@gmail.com>
2018-04-20 22:03:23 +0700

    Initial commit

---
+++ JniBitmapOperationsLibrary/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs-debug/JniBitmapOperationsLibrary/JniBitmapOperationsLibrary.o.d
@@ -0,0 +1,8 @@
+I:/OT/CameraView-master/JniBitmapOperationsLibrary/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs-debug/JniBitmapOperationsLibrary/JniBitmapOperationsLibrary.o: \
+  I:/OT/CameraView-master/JniBitmapOperationsLibrary/jni/JniBitmapOperationsLibrary.cpp \
+  C:/Users/luong/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/system/include\cstring \
+  C:/Users/luong/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/system/include\cstddef
+
+C:/Users/luong/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/system/include\cstring:
+
+C:/Users/luong/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/system/include\cstddef:
